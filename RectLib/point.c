88f2733f6487938533ee005612caac0fec59c7e8
Sylvain Thery <sylvain.thery@icube.unistra.fr>
2019-03-13 12:20:26 +0400

    test if

--- RectLib/point.c
+++ RectLib/point.c
@@ -1,5 +1,8 @@
 #include "point.h"
 
+extern inline float min(float x, float y);
+extern inline float max(float x, float y);
+
 float distance(Point pt1, Point pt2) {
   float dx = pt1.x - pt2.x;
   float dy = pt1.y - pt2.y;
